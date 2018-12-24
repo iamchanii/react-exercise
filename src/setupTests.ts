75a478eaf7a3658be085ea0612c6b44a51a9d14f
이찬희 <iamchanii-@naver.com>
2018-12-24 18:17:53 +0900

    test: 🧪 각 테스트 수행 후 cleanup 메소드 호출되도록 설정 추가

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,10 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-dom/extend-expect';
+import { cleanup } from 'react-testing-library';
 
 require('jest-localstorage-mock');
 
 configure({ adapter: new Adapter() });
+
+afterEach(cleanup);

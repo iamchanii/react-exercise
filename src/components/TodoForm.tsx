93904128cd2c99a83633a8f2b7d39534252441e3
이찬희 <iamchanii-@naver.com>
2018-12-24 18:21:07 +0900

    feat: TodoForm 컴포넌트에 state 인터페이스 추가 및 초기화

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -4,11 +4,19 @@ interface ITodoFormProps {
     onAdd: (title: string) => void;
 }
 
+interface ITodoFormState {
+    title: string;
+}
+
 /**
  * title 을 입력 받고, <App/> 에서 전달 된 onAdd 함수를 호출하는 Form 입니다.
  * state, ref 를 사용해야 할 것입니다.
  */
-class TodoForm extends Component<ITodoFormProps> {
+class TodoForm extends Component<ITodoFormProps, ITodoFormState> {
+    state: ITodoFormState = {
+        title: '',
+    };
+
     handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         // TODO: input 에 입력된 내용이 없으면 아무런 동작을 취하지 않는다.
